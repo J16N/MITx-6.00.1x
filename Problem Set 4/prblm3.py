bf5550b9219640444f93533fd215e51c3bd977b5
Tsubasa <J16N@users.noreply.github.com>
2021-03-09 14:57:01 +0000

    Added solutions to the problem set 4

---
+++ Problem Set 4/prblm3.py
@@ -0,0 +1,12 @@
+def isValidWord(word, hand, wordList):
+    """
+    Returns True if word is in the wordList and is entirely
+    composed of letters in the hand. Otherwise, returns False.
+
+    Does not mutate hand or wordList.
+   
+    word: string
+    hand: dictionary (string -> int)
+    wordList: list of lowercase strings
+    """
+    return word in wordList and all(word.count(l) <= hand.get(l, 0) for l in set(word))
